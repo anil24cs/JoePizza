f81e2dd00fe70ec16f4f40a742e9dc030274145e
Anil S <anil24cs@gmail.com>
2021-11-26 15:36:38 +0530

    first commit

---
+++ Pizza/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Pizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = null;
+            driver = new ChromeDriver(@"C:\dovercorp");
+
+            string url = "http://localhost:40871/";
+            driver.Navigate().GoToUrl(url);
+            
+
+
+        }
+    }
+}
